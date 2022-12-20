4f5db58ff66747b8fd22c50ffad5b16a76091857
Ibrahim-Khan88 <ebrahimkhanobak@gmail.com>
2022-12-20 12:51:35 +0600

    Registration process done

--- src/main/java/com/loofi/kyc/entity/CustomerKyc.java
+++ src/main/java/com/loofi/kyc/entity/CustomerKyc.java
@@ -4,13 +4,7 @@ import com.loofi.kyc.model.enums.IdType;
 
 import java.time.Instant;
 
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -27,6 +21,7 @@ public class CustomerKyc {
   private Long id;
   private String firstName;
   private String lastName;
+  @Column(unique=true)
   private String idNumber; // NID, passport, driving licence
   @Enumerated(EnumType.STRING)
   private IdType idType;
