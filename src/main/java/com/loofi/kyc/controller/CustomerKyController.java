7515afb9ec546243ae463ec35c5e2f6e4f55cb09
Ibrahim-Khan88 <ebrahimkhanobak@gmail.com>
2023-01-07 23:31:21 +0600

    implemented fetchCustomerByMobileNumber and fetchCustomerById api

--- src/main/java/com/loofi/kyc/controller/CustomerKyController.java
+++ src/main/java/com/loofi/kyc/controller/CustomerKyController.java
@@ -4,9 +4,12 @@ import com.loofi.kyc.entity.CustomerKyc;
 import com.loofi.kyc.repository.CustomerKycRepository;
 import com.loofi.kyc.service.CustomerKycService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/customerkyc")
@@ -32,5 +35,37 @@ public class CustomerKyController {
     @GetMapping("/idNumber/{idNumber}")
     boolean checkUniqueIdNumber(@PathVariable(value = "idNumber") String idNumber){
         return customerKycRepository.existsByIdNumber(idNumber);
+    }
+
+    @GetMapping("/mobile/{mobileNo}")
+    ResponseEntity<CustomerKyc> fetchCustomerByMobileNumber(@PathVariable(value = "mobileNo") String mobile){
+        Optional<CustomerKyc> customerKyc = customerKycRepository.findByMobileNo(mobile);
+        if(customerKyc.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(customerKyc.get());
+        else
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+
+    @GetMapping("/id/{id}")
+    ResponseEntity<CustomerKyc> fetchCustomerById(@PathVariable(value = "id") Long id){
+        Optional<CustomerKyc> customerKyc = customerKycRepository.findById(id);
+        if(customerKyc.isPresent())
+            return ResponseEntity.status(HttpStatus.OK).body(customerKyc.get());
+        else
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+    }
+    @GetMapping()
+    ResponseEntity<List<CustomerKyc>> fetchCustomers(){
+        List<CustomerKyc> customers = customerKycRepository.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(customers);
+    }
+
+    @DeleteMapping("/id/{id}")
+    ResponseEntity<Integer> deleteCustomerById(@PathVariable(value = "id") Long id){
+        if(customerKycRepository.existsById(id)){
+            customerKycRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.OK).body(null);
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
--- src/main/java/com/loofi/kyc/repository/CustomerKycRepository.java
+++ src/main/java/com/loofi/kyc/repository/CustomerKycRepository.java
@@ -3,10 +3,19 @@ package com.loofi.kyc.repository;
 import com.loofi.kyc.entity.CustomerKyc;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CustomerKycRepository extends JpaRepository<CustomerKyc, Integer> {
     Boolean existsByIdNumber(String idNumber);
 
+    boolean existsById(Long id);
+
+    Optional<CustomerKyc> findByMobileNo(String mobileNo);
+
     Optional<CustomerKyc> findByIdNumber(String idNumber);
+
+    Optional<CustomerKyc> findById(Long id);
+
+   void deleteById(Long id);
 }
